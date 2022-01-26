804dddd7d06c2b7c111a0d9a9413681913812fed
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-26 19:39:31 -0300

    item_1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,13 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age1) => {
-  const { residents } =
- 
+/* const getAnimalsOlderThan = (animal, age1) => {
 
 module.exports = getAnimalsOlderThan;
-
+ */
 /* { id }destruction. trás a informação que está dentro das chaves  */
-/* 2. Implemente a função getAnimalsOlderThan
+
+/*  2. Implemente a função getAnimalsOlderThan
 Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
 
 Observações técnicas
