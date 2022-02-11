c3486239d283dbd52645cf18872777cfdf24849d
Leandro Paiva <leandroapaiva@yahoo.com>
2022-02-11 17:34:05 -0300

    item 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,18 @@
-/* const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data'); */
+/* const { species } = require('../data/zoo_data'); */
+const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-/*   const func = data.employees.find((obj) => obj.id === id);
-  const animal = func.responsibleFor[0];
-  const test = species.find((element) => {}) */
+  const employeeID = data.employees.find((obj) => obj.id === id).responsibleFor[0];
+  const animalName = data.species.find((element) => element.id === employeeID);
+  let ageMax = animalName.residents[0];
+  animalName.residents.forEach((element) => {
+    if (element.age > ageMax.age) {
+      ageMax = element;
+    }
+  });
+  return Object.values(ageMax);
 }
+
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = getOldestFromFirstSpecies;
