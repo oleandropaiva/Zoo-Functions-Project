7ad5a6a733f6e2d16dc5d64b95dc3c72afa6003e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-02-11 15:33:07 -0300

    corrige destruction

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,12 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some(({ managers }) => managers.includes(id));
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(idMananger) {
   if (isManager(idMananger)) {
-    return data.employees.filter(({ managers }) => managers.includes(idMananger))
+    return data.employees.filter((element2) => element2.managers.includes(idMananger))
       .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
