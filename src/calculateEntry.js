b50eb6ed240b199d135963d94e97c505a7e6eaea
Leandro Paiva <leandroapaiva@yahoo.com>
2022-02-11 22:55:44 -0300

    getShedule

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  entrants.filter();
+  if (!entrants) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  return 
+  
+
 }
 
 function calculateEntry(entrants) {
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,47 @@
+const { hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-
+function animals(param) {
+  const animal = data.species;
+  return animal.find((element) => element.name === param).availability;
+}
+function dispon() {
+  const hour = data.hours;
+  const sem = Object.keys(hour);
+  sem.forEach((element) => {
+    const turno = hour[element];
+    if (element === 'Monday') {
+      hour[element] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      hour[element] = {
+        officeHour: `Open From ${turno.open}am until ${turno.close}pm`,
+        exhibition: data.species.filter((elemento) => elemento.availability.includes(element))
+          .map((elem) => elem.name),
+      };
+    }
+  });
+  return hour;
 }
 
+function returnDay(day) {
+  const dayAval = dispon();
+  const objDestruc = { [day]: dayAval[day] };
+  return objDestruc;
+}
+
+function getSchedule(scheduleTarget) {
+  const dayValid = Object.keys(data.hours).includes(scheduleTarget);
+  const animalValid = data.species.some((element) => element.name === scheduleTarget);
+  if (dayValid) {
+    return returnDay(scheduleTarget);
+  }
+  if (animalValid) {
+    return animals(scheduleTarget);
+  }
+  return dispon();
+}
+console.log(getSchedule(''));
 module.exports = getSchedule;
